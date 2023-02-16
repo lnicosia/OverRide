75b817ba9d723a656ffad9e2aa20415a279a5ef2
lnicosia <lnicosia@student.42.fr>
2023-02-16 17:50:58 +0100

    level05.

---
+++ level05/source.c
@@ -0,0 +1,15 @@
+int main(int argc, const char **argv, const char **envp)
+{
+  char s[100];
+  unsigned int i;
+
+  i = 0;
+  fgets(s, 100, stdin);
+  for ( i = 0; i < strlen(s); ++i )
+  {
+    if ( s[i] > 64 && s[i] <= 90 )
+      s[i] ^= 0x20; // Probablement toupper()
+  }
+  printf(s);
+  exit(0);
+}
