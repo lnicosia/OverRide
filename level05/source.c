4333b105315b0a65c4c1468f3bbeb9e732fa09fe
lnicosia <lnicosia@student.42.fr>
2023-03-02 12:31:57 +0100

    level05 polished.

--- level05/source.c
+++ level05/source.c
@@ -5,10 +5,10 @@ int main(int argc, const char **argv, const char **envp)
 
   i = 0;
   fgets(s, 100, stdin);
-  for ( i = 0; i < strlen(s); ++i )
+  for (i = 0; i < strlen(s); ++i)
   {
-    if ( s[i] > 64 && s[i] <= 90 )
-      s[i] ^= 0x20; // Probablement toupper()
+    if (s[i] > 64 && s[i] <= 90)
+      s[i] ^= 0x20; // Probablement tolower()
   }
   printf(s);
   exit(0);
